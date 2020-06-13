a9b7363e88cfae516fa23514cd3393d0632be8bc
oojackoo123 <66862296+oojackoo123@users.noreply.github.com>
2020-06-13 16:30:10 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -36,45 +36,45 @@ def callback():
     return 'OK'
 
 def getNews():
-	"""
-	建立一個抓最新消息的function
-	"""
-	import requests
-	import re
-	from bs4 import BeautifulSoup
+    """
+    建立一個抓最新消息的function
+    """
+    import requests
+    import re
+    from bs4 import BeautifulSoup
 
-	url = 'https://www.ettoday.net/news/focus/3C%E5%AE%B6%E9%9B%BB/'
-	r = requests.get(url)
-	reponse = r.text
+    url = 'https://www.ettoday.net/news/focus/3C%E5%AE%B6%E9%9B%BB/'
+    r = requests.get(url)
+    reponse = r.text
 
-	url_list = re.findall(r'<h3><a href="/news/[\d]*/[\d]*.htm" .*>.*</a>',reponse)
+    url_list = re.findall(r'<h3><a href="/news/[\d]*/[\d]*.htm" .*>.*</a>',reponse)
 
-	soup = BeautifulSoup(url_list[0])
-	url = 'https://fashion.ettoday.net/' + soup.find('a')['href']
-	title = soup.text
+    soup = BeautifulSoup(url_list[0])
+    url = 'https://fashion.ettoday.net/' + soup.find('a')['href']
+    title = soup.text
 
 
-	tmp = title + ': ' +url
-	return tmp
-	
+    tmp = title + ': ' +url
+    return tmp
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     # 傳送文字
     if event.message.text == '傳送文字':
         message = TextSendMessage(getNews())
-		 elif event.message.text == '傳送圖片':
+         elif event.message.text == '傳送圖片':
         message = ImageSendMessage(
             original_content_url='https://i.imgur.com/QPJ8A1b.png',
             preview_image_url='https://i.imgur.com/QPJ8A1b.png'
-			 elif event.message.text == '傳送貼圖':
+             elif event.message.text == '傳送貼圖':
         message = StickerSendMessage(
             package_id='1',
             sticker_id='1'
         )
-	else:
+    else:
         message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
 
-	
+    
 if __name__ == '__main__':
     app.run(debug=True)
