bedbad852cc48587959598c8c82d2bdc9d37ec02
oojackoo123 <66862296+oojackoo123@users.noreply.github.com>
2020-06-13 17:04:54 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('0SKGOAMS2iIokEnkEOoGd8l3gPSTqczICx77AckBV0aUS7SZFGa5NLifXwOmbrMLzR0jNYY2gD1R+t7qztDwyVw9QR1MwdycGtA505C69Jxt+Q1zcGPajNU/3eLJYPxUmcjfGFLO+CLX5wwDSebfCQdB04t89/1O/w1cDnyilFU=')
 
 # Channel secret 
-handler = WebhookHandler('266985951cd059799eaadd1d9a8c79d9')
+handler = WebhookHandler('Udf2c6bf11f169fa3820829560b76880f')
 
 
 @app.route("/callback", methods=['POST'])
@@ -56,21 +56,46 @@ def getNews():
 
     tmp = title + ': ' +url
     return tmp
+    
+def movie():
+    from bs4 import BeautifulSoup
+    import requests
+    
+    target_url = 'http://www.atmovies.com.tw/movie/next/0/'
+    print('Start parsing movie ...')
+    rs = requests.session()
+    res = rs.get(target_url, verify=False)
+    res.encoding = 'utf-8'
+    soup = BeautifulSoup(res.text, 'html.parser')
+    content = ""
+    for index, data in enumerate(soup.select('ul.filmListAll a')):
+        if index == 20:
+            break
+        title = data.text.replace('\t', '').replace('\r', '')
+        link = "http://www.atmovies.com.tw" + data['href']
+        content += '{}\n{}\n'.format(title, link)
+    return content
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    # 傳送文字
-    if event.message.text == '傳送文字':
+    # 傳送新聞
+    if event.message.text == '傳送新聞':
         message = TextSendMessage(getNews())
+    # 傳送圖片
     elif event.message.text == '傳送圖片':
         message = ImageSendMessage(
             original_content_url='https://i.imgur.com/QPJ8A1b.png',
             preview_image_url='https://i.imgur.com/QPJ8A1b.png'
+        )
+    # 傳送貼圖
     elif event.message.text == '傳送貼圖':
         message = StickerSendMessage(
             package_id='1',
             sticker_id='1'
         )
+    # 傳送電影 
+    elif event.message.text == '傳送電影':
+        message = TextSendMessage(movie()) 
     else:
         message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
