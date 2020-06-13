9cfaf7435de89de77bfe1b04a4d83cf04c0427a2
oojackoo123 <66862296+oojackoo123@users.noreply.github.com>
2020-06-13 16:42:22 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -62,11 +62,11 @@ def handle_message(event):
     # 傳送文字
     if event.message.text == '傳送文字':
         message = TextSendMessage(getNews())
-         elif event.message.text == '傳送圖片':
+    elif event.message.text == '傳送圖片':
         message = ImageSendMessage(
             original_content_url='https://i.imgur.com/QPJ8A1b.png',
             preview_image_url='https://i.imgur.com/QPJ8A1b.png'
-             elif event.message.text == '傳送貼圖':
+    elif event.message.text == '傳送貼圖':
         message = StickerSendMessage(
             package_id='1',
             sticker_id='1'
